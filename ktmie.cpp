a30f671b544b00c7af7b2058f9c619e08765cdb5
tatsuya-kawaguchi <83149185+tatsuya-kawaguchi@users.noreply.github.com>
2024-02-18 21:51:55 +0900

    Update ktmie.cpp

--- ktmie.cpp
+++ ktmie.cpp
@@ -142,51 +142,49 @@ int ktmie(
 )
 {
 
-    /////////////////////////////////
-    //
-    //  parameters for computation
-    //
-
+	/////////////////////////////////
+	//
+	//  parameters for computation
+	//
 	//	definition of the imaginary number, $i$
 	const complex<double> Z_ONE = complex<double>(0.0, 1.0);
 
 
 
-    //  store theta(rad)
-    vector<double> theta(N_ANG);
-    for(int i=0; i<theta.size(); i++) {
-        theta[i] = i * M_PI / (N_ANG-1);  //  radian 
-    }
+	//  store theta(rad)
+	vector<double> theta(N_ANG);
+	for(int i=0; i<theta.size(); i++) {
+		theta[i] = i * M_PI / (N_ANG-1);  //  radian 
+	}
+
+	//  relative complex refractive index
+	const complex<double> y = m * x;
+
+	//  iteration limit number
+	const int n_end = x+4*pow(x,1.0/3.0)+2 + 50;
+
+	//  if the delta S becomes very small, end the iteration
+	const double EPS = 1.0e-10;
+
+	//  Array of S1 and S2
+	S1.resize(theta.size(), complex<double>(0.0, 0.0));
+	S2.resize(theta.size(), complex<double>(0.0, 0.0));
 
 
-    //  relative complex refractive index
-    const complex<double> y = m * x;
 
-    //  iteration limit number
-    const int n_end = x+4*pow(x,1.0/3.0)+2 + 50;
-
-    //  if the delta S becomes very small, end the iteration
-    const double EPS = 1.0e-10;
-
-    //  Array of S1 and S2
-    S1.resize(theta.size(), complex<double>(0.0, 0.0));
-    S2.resize(theta.size(), complex<double>(0.0, 0.0));
-
-
-
-    /////////////////////////////////////////////////
-    //
+	/////////////////////////////////////////////////
+	//
 	// Riccati-Bessel functions and relating valiables
-    // for recurrence calcuration of a_n and b_n
-    //
-    //  psi_n_x,  psi_n_y; --- recurrence
-    //  dpsi_n_x, dpsi_n_y --- could be calc by psi
-    //  zeta_n_x        --- could be obtained from psi and chi
-    //  dzeta_n_x;      --- could be obtained from zeta = psi, chi
-    //
-    //  Consequently, psi, chi must be calcurated by recurrence formula
-    //
-    /////////////////////////////////////////////////
+	// for recurrence calcuration of a_n and b_n
+	//
+	//  psi_n_x,  psi_n_y; --- recurrence
+	//  dpsi_n_x, dpsi_n_y --- could be calc by psi
+	//  zeta_n_x        --- could be obtained from psi and chi
+	//  dzeta_n_x;      --- could be obtained from zeta = psi, chi
+	//
+	//  Consequently, psi, chi must be calcurated by recurrence formula
+	//
+	/////////////////////////////////////////////////
 
 	complex<double> psi_n_x;	//	== S_n(x)
 	complex<double> psi_n1_x;	//	psi_{n-1}
@@ -202,7 +200,6 @@ int ktmie(
 
 
 	//	set initial values
-
 	psi_n2_x = cos(x);	//	psi_{n-2}
 	psi_n1_x = sin(x);	//	psi_{n-1}
 
@@ -213,10 +210,6 @@ int ktmie(
 	chi_n1_x =  cos(x);	//	chi_{n-1}
 
 
-
-
-
-
     //////////////////////////////////////////////////////
     //
     //  angular functions
@@ -224,166 +217,143 @@ int ktmie(
     vector<double> pi_n, pi_n1, pi_n2;    //  pi_n(cos theta)
     //  tau_n is obtained from pi_n and pi_n1
 
-
-    //  set initial values
-    pi_n2.resize(theta.size(), 0.0);
-    pi_n1.resize(theta.size(), 1.0);
-
+	//  set initial values
+	pi_n2.resize(theta.size(), 0.0);
+	pi_n1.resize(theta.size(), 1.0);
 
 
-    /////////////////////////////////
-    //  master loop
-    /////////////////////////////////
-    for(int n=1; n<=n_end; n++) {
+	/////////////////////////////////
+	//  master loop
+	/////////////////////////////////
+	for(int n=1; n<=n_end; n++) {
 
-        cerr << "n=" << n << " of " << n_end << endl;
+		cerr << "n=" << n << " of " << n_end << endl;
 
 
-        ////////////////////////////////////////////////////////////////////
-        //
-        //  (1) set array of angular function
-        //
-        ////////////////////////////////////////////////////////////////////
+		////////////////////////////////////////////////////////////////////
+		//  (1) set array of angular function
+		////////////////////////////////////////////////////////////////////
 
-        if( n == 1 ) {
+		if( n == 1 ) {
+			pi_n = pi_n1;
+		} else {
+			for(int i=0; i<theta.size(); i++) {
+				const double x = cos(theta[i]); //  x is not a size parameter here, but a cos(theta).
+				pi_n[i] = (2.0*n-1.0)/(n-1.0) * x * pi_n1[i] - n/(n-1.0) * pi_n2[i];
+			}
+		}
 
-            pi_n = pi_n1;
+		////////////////////////////////////////////////////////////////////
+		//  (2) set coefficients, a_n and b_n
+		////////////////////////////////////////////////////////////////////
 
-        } else {
-
-            for(int i=0; i<theta.size(); i++) {
-                const double x = cos(theta[i]); //  x is not a size parameter here, but a cos(theta).
-                pi_n[i] = (2.0*n-1.0)/(n-1.0) * x * pi_n1[i] - n/(n-1.0) * pi_n2[i];
-            }
-
-        }
-
-        ////////////////////////////////////////////////////////////////////
-        //
-        //  (2) set coefficients, a_n and b_n
-        //
-        ////////////////////////////////////////////////////////////////////
-
-        //  For the calculation of coefficients, a_n and b_n, the following terms are required.
-        //
-        //  \psi_n(x)       psi_n_x
-        //  \psi_n(y)       psi_n_y
-        //  \psi'_n(x)      dpsi_n_x
-        //  \psi'_n(y)      dpsi_n_y
-        //  \zeta_n(x)      zeta_n_x
-        //  \zeta'_n(x)     dzeta_n_x
+		//  For the calculation of coefficients, a_n and b_n, the following terms are required.
+		//
+		//  \psi_n(x)       psi_n_x
+		//  \psi_n(y)       psi_n_y
+		//  \psi'_n(x)      dpsi_n_x
+		//  \psi'_n(y)      dpsi_n_y
+		//  \zeta_n(x)      zeta_n_x
+		//  \zeta'_n(x)     dzeta_n_x
 
 		//	(2-1) calc n'th values of recurrence formura
 		psi_n_x = (2.0*n-1.0)/(x) * psi_n1_x - psi_n2_x;	//	psi_1, psi_2, ...
 		psi_n_y = (2.0*n-1.0)/(y) * psi_n1_y - psi_n2_y;	//	psi_1, psi_2, ...
 		chi_n_x = (2.0*n-1.0)/(x) * chi_n1_x - chi_n2_x;	//	chi_1, chi_2, ...
-
-        // (2-2) calc other functions
+		
+		// (2-2) calc other functions
 		const complex<double> zeta_n1_x = psi_n1_x + Z_ONE * chi_n1_x;
 		const complex<double> zeta_n_x  = psi_n_x  + Z_ONE * chi_n_x ;
-
+		
 		const complex<double> dpsi_n_x  = psi_n1_x  - (double)n / x * psi_n_x;
 		const complex<double> dpsi_n_y  = psi_n1_y  - (double)n / y * psi_n_y;
 		const complex<double> dzeta_n_x = zeta_n1_x - (double)n / x * zeta_n_x;
+		
+		
+		//  (2-3) calc a_n and b_n
+		//  Definition of the coefficients is appeared in the book by van de Hulst, p.123
+		const complex<double> a_n
+		 = (    dpsi_n_y * psi_n_x - m * psi_n_y * dpsi_n_x) / (    dpsi_n_y * zeta_n_x - m * psi_n_y * dzeta_n_x);
+		
+		const complex<double> b_n
+		 = (m * dpsi_n_y * psi_n_x -     psi_n_y * dpsi_n_x) / (m * dpsi_n_y * zeta_n_x -     psi_n_y * dzeta_n_x);
+		
+		////////////////////////////////////////////////////////////////////
+		//  (3) calc n'th term of the S, in p.125
+		////////////////////////////////////////////////////////////////////
 
-
-        //  (2-3) calc a_n and b_n
-        //  Definition of the coefficients is appeared in the book by van de Hulst, p.123
-        const complex<double> a_n
-         = (    dpsi_n_y * psi_n_x - m * psi_n_y * dpsi_n_x) / (    dpsi_n_y * zeta_n_x - m * psi_n_y * dzeta_n_x);
-        
-        const complex<double> b_n
-         = (m * dpsi_n_y * psi_n_x -     psi_n_y * dpsi_n_x) / (m * dpsi_n_y * zeta_n_x -     psi_n_y * dzeta_n_x);
-
-
-        ////////////////////////////////////////////////////////////////////
-        //
-        //  (3) calc n'th term of the S, in p.125
-        //
-        ////////////////////////////////////////////////////////////////////
-
-        //  the n'th S
-        vector<complex<double> > S1_n(S1.size(), 0.0);
-        vector<complex<double> > S2_n(S2.size(), 0.0);
-
-        for(int i=0; i<theta.size(); i++) {
-            const double x = cos(theta[i]);
-            const double tau_n = n * x * pi_n[i] - (n+1.0)*pi_n1[i];
-
-            S1_n[i] = (2.0*n+1.0)/(n*(n+1.0)) * (a_n * pi_n[i] + b_n * tau_n );
-            S2_n[i] = (2.0*n+1.0)/(n*(n+1.0)) * (b_n * pi_n[i] + a_n * tau_n );
-        }
-
-
-
-        //////////////////////////////////////////////////////////////////
-        //  check the n'th term of S1 and S2
-        //  if the absolute values become small, terminate the loop of n
-        //////////////////////////////////////////////////////////////////
-        {
-            //  define unnamed function for the comparison of the complex values
-            auto complex_compare = [](complex<double> a, complex<double> b)
-            {
-                return abs(a) < abs(b);
-            };
-
-            const complex<double> max_S1_n = *std::max_element(S1_n.begin(), S1_n.end(), complex_compare);
-            const complex<double> max_S2_n = *std::max_element(S2_n.begin(), S2_n.end(), complex_compare);
-
-//            cerr << abs(max_S1_n) << " " << abs(max_S2_n) << endl;
-            if(fmax(abs(max_S1_n), abs(max_S2_n)) < EPS) {
-                cerr << "break loop of n" << endl;
-                break;
-            }
-        }
-
-
-
-        // update S1 and S2
-        for(int i=0; i<theta.size(); i++) {
-            S1[i] += S1_n[i];
-            S2[i] += S2_n[i];
-        }
-
-
-        ////////////////////////////////////////////////////////////////////
-        //
-        // (4-1) update Riccati-Bessel functions for the next loop
-        //
-        ////////////////////////////////////////////////////////////////////
-        {
-            psi_n2_x = psi_n1_x;
-            psi_n1_x = psi_n_x;
-    
-            psi_n2_y = psi_n1_y;
-            psi_n1_y = psi_n_y;
-
-            chi_n2_x = chi_n1_x;
-            chi_n1_x = chi_n_x;
-        }
-
-        ////////////////////////////////////////////////////////////////////
-        //
-        //  (4-2) update angular functions for the next loop
-        //
-        ////////////////////////////////////////////////////////////////////
-        if( n >= 2 ) {
-            pi_n2 = pi_n1;
-            pi_n1 = pi_n;
-        }
-
-        if(0) { 
-            //  write data
-            cout << "n=," << n << ",,";
-            for(int i=0; i<theta.size(); i++) {
-    //            cout << theta[i]*180/M_PI << "," << log10(norm(S1[i])) << "," << log10(norm(S2[i])) << "\n";
-                cout << log10(norm(S1[i])) << ",";
-    //            cout << log10(norm(S2[i])) << ",";
-            }
-            cout << "\n";
-        }
-
-    }
-
+		//  the n'th S
+		vector<complex<double> > S1_n(S1.size(), 0.0);
+		vector<complex<double> > S2_n(S2.size(), 0.0);
+		
+		for(int i=0; i<theta.size(); i++) {
+			const double x = cos(theta[i]);
+			const double tau_n = n * x * pi_n[i] - (n+1.0)*pi_n1[i];
+		
+			S1_n[i] = (2.0*n+1.0)/(n*(n+1.0)) * (a_n * pi_n[i] + b_n * tau_n );
+			S2_n[i] = (2.0*n+1.0)/(n*(n+1.0)) * (b_n * pi_n[i] + a_n * tau_n );
+		}
+		
+		//////////////////////////////////////////////////////////////////
+		//  check the n'th term of S1 and S2
+		//  if the absolute values become small, terminate the loop of n
+		//////////////////////////////////////////////////////////////////
+		{
+			//  define unnamed function for the comparison of the complex values
+			auto complex_compare = [](complex<double> a, complex<double> b)
+			{
+				return abs(a) < abs(b);
+			};
+	
+			const complex<double> max_S1_n = *std::max_element(S1_n.begin(), S1_n.end(), complex_compare);
+			const complex<double> max_S2_n = *std::max_element(S2_n.begin(), S2_n.end(), complex_compare);
+	
+			if(fmax(abs(max_S1_n), abs(max_S2_n)) < EPS) {
+				cerr << "break loop of n" << endl;
+				break;
+			}
+		}
+	
+		// update S1 and S2
+		for(int i=0; i<theta.size(); i++) {
+			S1[i] += S1_n[i];
+			S2[i] += S2_n[i];
+		}
+		
+		////////////////////////////////////////////////////////////////////
+		// (4-1) update Riccati-Bessel functions for the next loop
+		////////////////////////////////////////////////////////////////////
+		{
+			psi_n2_x = psi_n1_x;
+			psi_n1_x = psi_n_x;
+		
+			psi_n2_y = psi_n1_y;
+			psi_n1_y = psi_n_y;
+		
+			chi_n2_x = chi_n1_x;
+			chi_n1_x = chi_n_x;
+		}
+		
+		////////////////////////////////////////////////////////////////////
+		//  (4-2) update angular functions for the next loop
+		////////////////////////////////////////////////////////////////////
+		if( n >= 2 ) {
+			pi_n2 = pi_n1;
+			pi_n1 = pi_n;
+		}
+		
+		if(0) { 
+			//  write data
+			cout << "n=," << n << ",,";
+			for(int i=0; i<theta.size(); i++) {
+//				cout << theta[i]*180/M_PI << "," << log10(norm(S1[i])) << "," << log10(norm(S2[i])) << "\n";
+				cout << log10(norm(S1[i])) << ",";
+//				cout << log10(norm(S2[i])) << ",";
+			}
+			cout << "\n";
+		}
+	
+	}
+	
     return 0;
 }
